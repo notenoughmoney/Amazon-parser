fd5d77e2129e1b7c1e9b8fcd01d5e08b745d30ee
user <user@I21013401739>
2023-03-27 14:51:15 +0800

    Добавьте файлы проекта.

---
+++ newParser/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace newParser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string html = Utils.getHTML(textBox1.Text);
+            DataTable table = Utils.getData(html);
+
+            foreach (DataRow row in table.Rows)
+            {
+                ListViewItem item = new ListViewItem(row[0].ToString());
+                for (int i = 1; i < table.Columns.Count; i++)
+                {
+                    item.SubItems.Add(row[i].ToString());
+                }
+                listView1.Items.Add(item);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+            listView1.Columns.Add("Название книги", 400);
+            listView1.Columns.Add("Автор", 40);
+            listView1.Columns.Add("Оценка", 40);
+            listView1.Columns.Add("Дата выхода", 40);
+            listView1.Columns.Add("Цена", 40);
+        }
+    }
+}
---
+++ newParser/utils.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace newParser
+{
+    class Utils
+    {
+        public static string getHTML(string query) {
+            WebClient client = new WebClient();
+            client.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0");
+            client.Headers.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8");
+            client.Headers.Add("Accept-Encoding", "br");
+            client.Headers.Add("Accept-Language", "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3");
+
+            string language = query;
+            string baseUrl = "https://www.amazon.com/s?k=" + language;
+            int currentPage = 1;
+            string url = baseUrl + "&page=" + currentPage;
+            Stream data = client.OpenRead(url);
+            StreamReader reader = new StreamReader(data);
+            return reader.ReadToEnd();
+        }
+
+        public static DataTable getData(string html) {
+            Regex r = new Regex(@"<span.*a-size-base-plus a-color-base a-text-normal[^>]*>(.*?)</span>.*(?:<a.*a-link-normal s-underline-text s-underline-link-text s-link-style[^>]*>|<span.*a-size-base[^>]*>|<span>)([^<]*)(?:</a>|</span>).*<span.*a-size-base[^>]*>(\d.\d)</span>.*([$]\d+\.\d+)");
+            int gotCount = 0;
+
+            DataTable table = new DataTable();
+            table.Columns.Add("Name", typeof(string));
+            table.Columns.Add("Author", typeof(string));
+            table.Columns.Add("Rating", typeof(string));
+            table.Columns.Add("Price", typeof(string));
+
+            foreach (Match m in r.Matches(html))
+            {
+                gotCount++;
+                DataRow row;
+            
+                row = table.NewRow();
+                row[0] = m.Groups[1];
+                row[1] = m.Groups[2];
+                row[2] = m.Groups[3];
+                row[3] = m.Groups[4];
+                table.Rows.Add(row);
+            }
+            return table;
+        }
+    }
+}
