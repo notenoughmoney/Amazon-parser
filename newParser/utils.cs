b017417eaf29f66b4361b667c017e2a9c9970825
ponchik009 <lolikonru@mail.ru>
2023-03-28 11:04:35 +0800

    regex for book url

--- newParser/utils.cs
+++ newParser/utils.cs
@@ -28,7 +28,7 @@ namespace newParser
         }
 
         public static async Task<DataTable> getData(string query, int amount = 10) {
-            Regex r = new(@"<span.*a-size-base-plus a-color-base a-text-normal[^>]*>(.*?)</span>.*(?:<a.*a-link-normal s-underline-text s-underline-link-text s-link-style[^>]*>|<span.*a-size-base[^>]*>|<span>)([^<]*)(?:</a>|</span>).*<span.*a-size-base[^>]*>(\d.\d)</span>.*([$]\d+\.\d+)");
+            Regex r = new(@"<a.*a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal.*href=[^\s](.*)""><span.*a-size-base-plus a-color-base a-text-normal[^>]*>(.*?)</span>.*(?:<a.*a-link-normal s-underline-text s-underline-link-text s-link-style[^>]*>|<span.*a-size-base[^>]*>|<span>)([^<]*)(?:</a>|</span>).*<span.*a-size-base[^>]*>(\d.\d)</span>.*([$]\d+\.\d+)");
             int gotCount = 0;
             int currentPage = 1;
 
@@ -46,12 +46,15 @@ namespace newParser
                 {
                     gotCount++;
                     DataRow row;
-            
+
+                    string bookUrl = "https://www.amazon.com" + m.Groups[1];
+
                     row = table.NewRow();
-                    row[0] = m.Groups[1];
-                    row[1] = m.Groups[2];
-                    row[2] = m.Groups[3];
-                    row[3] = m.Groups[4]
+
+                    row[0] = m.Groups[2];
+                    row[1] = m.Groups[3];
+                    row[2] = m.Groups[4];
+                    row[3] = m.Groups[5]
                         .ToString()
                         .Replace(".", ",")
                         .Substring(1);
