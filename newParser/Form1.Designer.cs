9024cf5d088e49bf042341a9b3ba068003e3cdb1
Dmitrii Kryazhev <Kr.dmitry@mail.ru>
2023-03-28 11:45:30 +0800

    Добавлена возможность получать имя книги...

--- newParser/Form1.Designer.cs
+++ newParser/Form1.Designer.cs
@@ -37,28 +37,25 @@ namespace newParser
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(14, 17);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Location = new System.Drawing.Point(12, 13);
             this.textBox1.Name = "textBox1";
             this.textBox1.PlaceholderText = "Введите название книги...";
-            this.textBox1.Size = new System.Drawing.Size(734, 27);
+            this.textBox1.Size = new System.Drawing.Size(643, 23);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(777, 17);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.Location = new System.Drawing.Point(680, 13);
             this.textBox2.Name = "textBox2";
             this.textBox2.PlaceholderText = "Кол-во";
-            this.textBox2.Size = new System.Drawing.Size(176, 27);
+            this.textBox2.Size = new System.Drawing.Size(154, 23);
             this.textBox2.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(992, 15);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Location = new System.Drawing.Point(868, 11);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(227, 31);
+            this.button1.Size = new System.Drawing.Size(199, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "Найти";
             this.button1.UseVisualStyleBackColor = true;
@@ -68,26 +65,26 @@ namespace newParser
             // 
             this.listView1.BackColor = System.Drawing.Color.White;
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(14, 79);
-            this.listView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listView1.Location = new System.Drawing.Point(12, 59);
+            this.listView1.MultiSelect = false;
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(1205, 628);
+            this.listView1.Size = new System.Drawing.Size(1055, 472);
             this.listView1.TabIndex = 3;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView1_ColumnClick);
+            this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseClick);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(1233, 724);
+            this.ClientSize = new System.Drawing.Size(1079, 543);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Парсер книг";
             this.Load += new System.EventHandler(this.Form1_Load);
